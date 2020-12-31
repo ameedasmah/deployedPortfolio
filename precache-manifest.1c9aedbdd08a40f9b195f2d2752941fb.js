bb5da3b31143be46499ba0af8e881a6b2291bb43
ameedasmah <ameed.asmah@hotmail.com>
2020-12-31 09:52:57 +0200

    Updates

---
+++ precache-manifest.1c9aedbdd08a40f9b195f2d2752941fb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "56cebf48631646b2907c9f02d0d6510a",
+    "url": "/React-Portfolio-Template/index.html"
+  },
+  {
+    "revision": "1df6d9deeb72c130fc42",
+    "url": "/React-Portfolio-Template/static/css/main.ec818708.chunk.css"
+  },
+  {
+    "revision": "602b07b284fd5c9905ce",
+    "url": "/React-Portfolio-Template/static/js/2.755097e9.chunk.js"
+  },
+  {
+    "revision": "0d379520bf19be6441d9b3fd72145865",
+    "url": "/React-Portfolio-Template/static/js/2.755097e9.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1df6d9deeb72c130fc42",
+    "url": "/React-Portfolio-Template/static/js/main.fb164967.chunk.js"
+  },
+  {
+    "revision": "5892fe25b6e95855d46e",
+    "url": "/React-Portfolio-Template/static/js/runtime-main.76593dbb.js"
+  }
+]);
